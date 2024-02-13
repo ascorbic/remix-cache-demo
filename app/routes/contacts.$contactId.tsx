5be737093d8990d5d85654091ddebf9882e24e0b
Matt Kane <matt.kane@netlify.com>
2024-02-13 09:24:39 +0000

    Use contacts demo

---
+++ app/routes/contacts.$contactId.tsx
@@ -0,0 +1,116 @@
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Form, useFetcher, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import type { ContactRecord } from "../data";
+import { getContact, updateContact } from "../data";
+
+export const action = async ({ params, request }: ActionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const formData = await request.formData();
+  const contact = await updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+  // const { purgeCache } = await import("@netlify/functions");
+
+  // purgeCache({
+  //   tags: ["contacts", `contact-${contact.id}`],
+  // })
+  return contact
+};
+
+export const loader = async ({ params }: LoaderArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ contact }, {
+    headers: {
+      // Cache for a year in the CDN, but always revalidate in the browser
+      "Cache-Control": "public, max-age=0, s-maxage: 31536000",
+      // Revalidate whenever the products change
+      "Cache-Tag": "contacts",
+    },
+  });
+};
+
+export default function Contact() {
+  const { contact } = useLoaderData<typeof loader>();
+
+  return (
+    <div id="contact">
+      <div>
+        <img
+          alt={`${contact.first} ${contact.last} avatar`}
+          key={contact.avatar}
+          src={contact.avatar}
+        />
+      </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter ? (
+          <p>
+            <a href={`https://twitter.com/${contact.twitter}`}>
+              {contact.twitter}
+            </a>
+          </p>
+        ) : null}
+
+        {contact.notes ? <p>{contact.notes}</p> : null}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+
+          <Form
+            action="destroy"
+            method="post"
+            onSubmit={(event) => {
+              const response = confirm(
+                "Please confirm you want to delete this record.",
+              );
+              if (!response) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Favorite({ contact }: { contact: Pick<ContactRecord, "favorite"> }) {
+  const fetcher = useFetcher();
+  const favorite = fetcher.formData
+    ? fetcher.formData.get("favorite") === "true"
+    : contact.favorite;
+
+  return (
+    <fetcher.Form method="post">
+      <button
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+        name="favorite"
+        value={favorite ? "false" : "true"}
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </fetcher.Form>
+  );
+}
--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,7 @@ import { config } from "@netlify/remix-adapter";
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ...(process.env.NODE_ENV === "production" ? config : undefined),
+  browserNodeBuiltinsPolyfill: { modules: { stream: true, process: true, util: true } }
   // This works out of the box with the Netlify adapter, but you can
   // add your own custom config here if you want to.
   //
