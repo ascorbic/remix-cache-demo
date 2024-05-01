ba3320b5a9fa6471d20170982fc23438f0ccebd2
Matt Kane <matt.kane@netlify.com>
2024-05-01 09:40:04 +0100

    Back to old data handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,13 @@
 import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 import { vitePlugin as remix } from "@remix-run/dev";
-import slugify from "@sindresorhus/slugify";
-import { mkdir, writeFile } from "node:fs/promises";
-import { join } from "node:path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import contactData from "./app/contact-data.json" assert { type: "json" };
 
 export default defineConfig({
   plugins: [
     remix({
       future: {
         unstable_singleFetch: true,
-      },
-      buildEnd: async () => {
-        console.log("build end");
-        // do stuff
-        const blobDeployDir = join(".netlify", "blobs", "deploy");
-        await mkdir(blobDeployDir, { recursive: true });
-        for (const contact of contactData.contacts) {
-          const id = slugify(`${contact.first}-${contact.last}`);
-          await writeFile(
-            join(blobDeployDir, id),
-            JSON.stringify({ id, ...contact })
-          );
-        }
       },
     }),
     netlifyPlugin(),
